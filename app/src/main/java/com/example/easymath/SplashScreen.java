ae7403dcf2ff977462612c92be9f754d01f4bef7
AngelikaJustine <fay.ariana@gmail.com>
2019-12-04 19:08:33 +0700

    Landing Page + Splash Screen

---
+++ app/src/main/java/com/example/easymath/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.easymath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashScreenTimeOut = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        logo = findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, splashScreenTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.splashanimation);
+        logo.startAnimation(myanim);
+
+    }
+
+
+}
