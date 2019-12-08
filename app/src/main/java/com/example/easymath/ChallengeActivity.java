7ea49aa9297cfc337ef8fc4454ee6a9435310303
AngelikaJustine <fay.ariana@gmail.com>
2019-12-08 18:13:07 +0700

    Update Challenge after finish game

--- app/src/main/java/com/example/easymath/ChallengeActivity.java
+++ app/src/main/java/com/example/easymath/ChallengeActivity.java
@@ -90,7 +90,9 @@ public class ChallengeActivity extends AppCompatActivity {
 
     private void gameOver() {
         Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+        finish();
     }
 
     private void updateCountDownText() {
